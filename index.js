68aa3d886ef1b85169a16db03f34d0749cb842eb
Sergey <GreyTob@inbox.ru>
2021-08-03 08:24:26 +0500

    body-parser

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const { response } = require('express')
 const express = require('express')
 const { request } = require('http')
 
+const bodyParser = require('body-parser')
+
 const app = express()
 
 //файлы ejs по умолчанию
 app.set('view engine', 'ejs')
-
-//укажем ститические файлы(напр чтобы подключить css)
+//укажем статические файлы(напр.: чтобы подключить css)
 app.use(express.static('public'))
+//для получения данных в request.body
+app.use(bodyParser.urlencoded({ extends: true }))
 
 app.get('/', (request, response) => {
   response.render('index')
@@ -20,6 +23,13 @@ app.get('/', (request, response) => {
 // app.get('/about', (request, response) => {
 //   response.end('about page')
 // })
+
+app.post('/', (request, response) => {
+  console.log(request.body)
+  const { city } = request.body
+  console.log(city)
+  response.render('index')
+})
 
 //port 3000, callback
 app.listen(3000, () => {
